fbbec52f1f0a7894aba0349cd3ab4a7f3b1571e9
Vlad Bazhanov <vb@compliancetech.ru>
2020-04-16 16:40:58 +0300

    Updates

---
+++ precache-manifest.e5868861209599db3a78812865de378e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609e46360c3c01fe07f9b060c01a093d",
+    "url": "/todos_ts/index.html"
+  },
+  {
+    "revision": "8ebada4ba2d91ff6c9d8",
+    "url": "/todos_ts/static/css/main.6da6fecd.chunk.css"
+  },
+  {
+    "revision": "710984333240bc202b2f",
+    "url": "/todos_ts/static/js/2.ead44af0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos_ts/static/js/2.ead44af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ebada4ba2d91ff6c9d8",
+    "url": "/todos_ts/static/js/main.6d5cad5c.chunk.js"
+  },
+  {
+    "revision": "16421a212740dd9b6aaa",
+    "url": "/todos_ts/static/js/runtime-main.a4d02887.js"
+  }
+]);
